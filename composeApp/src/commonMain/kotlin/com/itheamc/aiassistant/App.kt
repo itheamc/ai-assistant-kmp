47bf7e763237cea2e890ef781cd9202c4947a679
itheamc <itheamc@gmail.com>
2025-12-25 23:27:34 +0545

    feat: enhanced app composable

--- composeApp/src/commonMain/kotlin/com/itheamc/aiassistant/App.kt
+++ composeApp/src/commonMain/kotlin/com/itheamc/aiassistant/App.kt
@@ -1,47 +1,52 @@
 package com.itheamc.aiassistant
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.navigation.compose.rememberNavController
+import com.itheamc.aiassistant.core.di.koinConfig
+import com.itheamc.aiassistant.ui.RootLayout
+import com.itheamc.aiassistant.ui.local_providers.LocalNavController
+import com.itheamc.aiassistant.ui.theme.AiAssistantAppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinMultiplatformApplication
+import org.koin.core.annotation.KoinExperimentalAPI
 
-import aiassistant.composeapp.generated.resources.Res
-import aiassistant.composeapp.generated.resources.compose_multiplatform
-
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                }
-            }
+fun App(
+    onKoinConfigured: () -> Unit = {},
+) {
+    KoinMultiplatformApplication(
+        config = koinConfig
+    ) {
+        AiAssistantApp(
+            onKoinConfigured = onKoinConfigured,
+        )
+    }
+}
+
+@Composable
+private fun AiAssistantApp(
+    onKoinConfigured: () -> Unit = {},
+) {
+
+    val navController = rememberNavController()
+
+    LaunchedEffect(Unit) {
+        onKoinConfigured.invoke()
+    }
+
+    CompositionLocalProvider(
+        LocalNavController provides navController,
+    ) {
+        AiAssistantAppTheme {
+            RootLayout(
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
